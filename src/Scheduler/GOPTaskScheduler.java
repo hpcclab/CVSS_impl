675897be8f7de1a3550340cf3725fcd039fe3494
cxd9974 <Chavit.Username.toString@Gmail.com>
2018-01-19 15:54:28 -0600

    move from pc to laptop

--- src/Scheduler/GOPTaskScheduler.java
+++ src/Scheduler/GOPTaskScheduler.java
@@ -6,33 +6,31 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
+
 class request{ //fill every thing for lvl1 mapping constructor, skip resolution for lvl2 mapping, ...
     private final String command;
-    private final String  Setting_resWidth;
-    private final String  Setting_resHeight;
-    private final String  Setting_absPath;
+    private final String  param_resWidth;
+    private final String  param_resHeight;
+    private final String Path;
 
     //constructor with StreamGOP and intended level of matching
     request(StreamGOP original,int level){
         if(level==3) {
-            Setting_absPath=original.userSetting.absPath; //match video segment
+            Path=original.getPath(); //match video segment
             command="";
-            Setting_resHeight="";
-            Setting_resWidth="";
+            param_resHeight="";
+            param_resWidth="";
         }else if(level==2){
-            Setting_absPath=original.userSetting.absPath;
+            Path=original.getPath();
             command=original.command; //match command
-            Setting_resHeight="";
-            Setting_resWidth="";
-        }else{ //level 1
-            Setting_absPath=original.userSetting.absPath;
+            param_resHeight="";
+            param_resWidth="";
+        }else{ //level 1, level 3 is not yet supported
+            Path=original.getPath();
             command=original.command;
-            Setting_resHeight=original.userSetting.resHeight; //match resolution too
-            Setting_resWidth=original.userSetting.resWidth;
+            param_resHeight=original.userSetting.resHeight; //match resolution too
+            param_resWidth=original.userSetting.resWidth;
 
         }
 
@@ -41,9 +39,9 @@ class request{ //fill every thing for lvl1 mapping constructor, skip resolution 
         return new HashCodeBuilder(17, 31). // two randomly chosen prime numbers
                 // if deriving: appendSuper(super.hashCode()).
                         append(command).
-                        append(Setting_resWidth).
-                        append(Setting_resHeight).
-                        append(Setting_absPath).
+                        append(param_resWidth).
+                        append(param_resHeight).
+                        append(Path).
                         toHashCode();
     }
 
@@ -59,9 +57,9 @@ class request{ //fill every thing for lvl1 mapping constructor, skip resolution 
         request X=(request)obj;
         return new EqualsBuilder().
                 append(command,X.command).
-                append(Setting_resWidth,X.Setting_resWidth).
-                append(Setting_resHeight,X.Setting_resHeight).
-                append(Setting_absPath,X.Setting_absPath).
+                append(param_resWidth,X.param_resWidth).
+                append(param_resHeight,X.param_resHeight).
+                append(Path,X.Path).
                 isEquals();
         }
 }
@@ -70,8 +68,8 @@ public class GOPTaskScheduler {
     private PriorityQueue<StreamGOP> Batchqueue=new PriorityQueue<StreamGOP>();
     private int working=0;
     private HashMap<request,StreamGOP> LV1map_pending=new HashMap<request,StreamGOP>();;
-    private HashMap<request,StreamGOP> LV2map_pending=new HashMap<request,StreamGOP>(); //level2's request record skip resolution so more matches
-
+    //private HashMap<request,List<StreamGOP>> LV2map_pending=new HashMap<request,List<StreamGOP>>(); //level2's request record skip resolution so more matches
+    private HashMap<request,StreamGOP> LV2map_pending=new HashMap<request,StreamGOP>();
     public static ArrayList<VMinterface> VMinterfaces =new ArrayList<VMinterface>();
     public GOPTaskScheduler(){
         if(ServerConfig.mapping_mechanism.equalsIgnoreCase("ShortestQueueFirst")){
@@ -95,20 +93,31 @@ public class GOPTaskScheduler {
         for(StreamGOP X:ST.streamGOPs) {
             //HOLD UP! check for duplication first
             request aRequestlvl1 = new request(X,1); //= ... derive from X
+            System.out.println(X.userSetting.absPath +" "+X.userSetting.videoname);
             if (LV1map_pending.containsKey(aRequestlvl1)) {
-                System.out.println("match level 1 (exactly the same request) -> dropping this request");
+                System.out.println("match level 1 (exactly the same request) -> dropping this request, no question!");
+                //don't even need to check if it is not null or state is not dispatched
             }else{
-                LV1map_pending.put(aRequestlvl1,X); //add to the level0 map
-                //check lvl2
+                LV1map_pending.put(aRequestlvl1,X); //add to the level0 map, no need to be a list as there aren't any object that is not merged
+                //check level 2
                 request aRequestlvl2 = new request(X,2);
                 if (LV2map_pending.containsKey(aRequestlvl2)) {
                     System.out.println("match level 2 (request on the same video and same command, dif resolution");
                     // do merging
-                    System.out.println("MERGE!");
+                    System.out.println("MERGE or not?");
+
+                    // IMPORTANT, if we don't merge, redo LV2map_pending.put(aRequestlvl2,X); to say we are the candidate for merge not the one we fail to merge with
+
                     //
                 }else{
+                   // ArrayList<StreamGOP> listofGOP=new ArrayList<StreamGOP>();
+                   // listofGOP.add(X);
+                   // LV2map_pending.put(aRequestlvl2,listofGOP);
                     LV2map_pending.put(aRequestlvl2,X);
+                    // check level 3
 
+
+                    //...
                     Batchqueue.add(X);
                 }
             }
